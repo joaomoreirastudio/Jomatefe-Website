fc219a2c8baf63a163afc2078d9b4ba8525a2d93
João Moreira <joaomoreirastudio@gmail.com>
2025-05-13 16:00:44 +0100

    fix: Deleted context files not used

--- src/Context/ProductListContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useState } from "react";
-
-interface ProductListContextValue {
-    setCategorySelected: (category: string) => void;
-}
-
-const ProductListContext = createContext<ProductListContextValue | null>(null);
-
-export default ProductListContext;
